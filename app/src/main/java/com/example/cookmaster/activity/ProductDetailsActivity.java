0a86148fa77f75d8cd9bcff193624feb216cd741
AriesKira <linharesrodrigues.gil@gmail.com>
2023-07-05 16:20:00 +0200

    FINISHED PRODUCTS Redirect to shop added

--- app/src/main/java/com/example/cookmaster/activity/ProductDetailsActivity.java
+++ app/src/main/java/com/example/cookmaster/activity/ProductDetailsActivity.java
@@ -3,10 +3,13 @@ package com.example.cookmaster.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cookmaster.R;
 import com.example.cookmaster.Utils.ImageHandler;
@@ -18,6 +21,11 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private Button buyButton,returnButton;
     private TextView productPrice, productDescription, productName;
     private ShopItem product;
+
+    private static final String BASE_URL = "https://cookorama.fr/boutique/produit/";
+    private String noBrowserError = String.valueOf(R.string.no_browser_error);
+    private String url;
+    private Uri webpage;
 
 
     @Override
@@ -36,6 +44,8 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         product = (ShopItem) intent.getSerializableExtra("product");
+        this.url = BASE_URL + product.getId();
+        this.webpage = Uri.parse(url);
 
         productDescription.setText(product.getDescription());
         productName.setText(product.getName());
@@ -44,6 +54,15 @@ public class ProductDetailsActivity extends AppCompatActivity {
 
         returnButton.setOnClickListener(v -> {
             finish();
+        });
+
+        buyButton.setOnClickListener(v -> {
+            Intent buyIntent = new Intent(Intent.ACTION_VIEW, webpage);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(buyIntent);
+            } else {
+                Toast.makeText(this, noBrowserError + url, Toast.LENGTH_SHORT).show();
+            }
         });
 
     }
